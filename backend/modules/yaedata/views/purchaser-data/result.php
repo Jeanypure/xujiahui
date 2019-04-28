8a2c9ac4a349f7eab2b94cda779731a53ad8ec4e
Pure <wakakajingjing@gmail.com>
2019-04-28 14:08:09 +0800

    own source compute add

--- backend/modules/yaedata/views/purchaser-data/result.php
+++ backend/modules/yaedata/views/purchaser-data/result.php
@@ -6,115 +6,149 @@
  * Time: 10:59
  */
 use kartik\grid\GridView;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use kartik\daterange\DateRangePicker;
+use kartik\widgets\ActiveForm;
 
-echo GridView::widget([
-    'dataProvider' => $dataProvider,
-    'showPageSummary'=>true,
-    'pjax'=>true,
-    'export' => false,
-    'striped'=>true,
-    'hover'=>true,
-    'panel'=>['type'=>'primary', 'heading'=>'采购开发产品统计--来源 自主开发+销售推荐'],
-    'columns'=>[
-        ['class'=>'kartik\grid\SerialColumn'],
-
-        [
-            'attribute'=>'purchaser',
-            'pageSummary'=>'Page Summary',
-            'pageSummaryOptions'=>['class'=>'text-right text-warning'],
-        ],
-       [
-            'attribute'=>'reject',
-            'label'=>'拒绝',
-            'width'=>'150px',
-            'hAlign'=>'right',
-            'format'=>['decimal', 0],
-            'pageSummary'=>true
-        ],
-        [
-            'attribute'=>'get',
-            'label'=>'拿样',
-            'width'=>'150px',
-            'hAlign'=>'right',
-            'format'=>['decimal', 0],
-            'pageSummary'=>true
-        ],
-        [
-            'attribute'=>'need',
-            'label'=>'需议价或谈其他条件',
-            'width'=>'150px',
-            'hAlign'=>'right',
-            'format'=>['decimal', 0],
-            'pageSummary'=>true
-        ],
-        [
-            'attribute'=>'undo',
-            'label'=>'未评审',
-            'width'=>'150px',
-            'hAlign'=>'right',
-            'format'=>['decimal', 0],
-            'pageSummary'=>true
-        ],
-        [
-            'attribute'=>'direct',
-            'label'=>'直接下单',
-            'width'=>'150px',
-            'hAlign'=>'right',
-            'format'=>['decimal', 0],
-            'pageSummary'=>true
-        ],[
-            'attribute'=>'season',
-            'label'=>'季节产品推迟',
-            'width'=>'150px',
-            'hAlign'=>'right',
-            'format'=>['decimal', 0],
-            'pageSummary'=>true
-        ],
-        [
-            'class'=>'kartik\grid\FormulaColumn',
-            'header'=>'产品总数',
-            'value'=>function ($model, $key, $index, $widget) {
-                $p = compact('model', 'key', 'index');
-                return $widget->col(2, $p) + $widget->col(3, $p)+
-                    $widget->col(4, $p)+ $widget->col(5, $p)+ $widget->col(6, $p)+ $widget->col(7, $p);
-            },
-            'mergeHeader'=>true,
-            'width'=>'150px',
-            'hAlign'=>'right',
-            'format'=>['decimal', 0],
-            'pageSummary'=>true
-        ],
-        [
-            'class'=>'kartik\grid\FormulaColumn',
-            'header'=>'拒绝率%',
-            'value'=>function ($model, $key, $index, $widget) {
-                $p = compact('model', 'key', 'index');
-                return $widget->col(2, $p) / $widget->col(8, $p) *100;
-            },
-            'mergeHeader'=>true,
-            'width'=>'150px',
-            'hAlign'=>'right',
-            'format'=>['decimal', 2],
-//            'pageSummary'=>true,
-            'pageSummaryFunc'=>GridView::F_AVG,
-        ],
-        [
-            'class'=>'kartik\grid\FormulaColumn',
-            'header'=>'成功率%',
-            'value'=>function ($model, $key, $index, $widget) {
-                $p = compact('model', 'key', 'index');
-                return ($widget->col(3, $p)+ $widget->col(6, $p))/ $widget->col(8, $p) *100;
-            },
-            'mergeHeader'=>true,
-            'width'=>'150px',
-            'hAlign'=>'right',
-            'format'=>['decimal', 2],
-//            'pageSummary'=>true,
-            'pageSummaryFunc'=>GridView::F_AVG,
-
-        ],
-    ],
+?>
+<?php
+    $form = ActiveForm::begin();
+echo '<div class="form-group"><div class="row">';
+echo '<div class="col-md-4"><label class="control-label">Date Range</label>';
+echo '<div class="drp-container">';
+echo DateRangePicker::widget([
+    'name'=>'date_range_3',
+    'value'=>date('Y-m-d',strtotime('-90 day')) . ' - '. date('Y-m-d') ,
+    'presetDropdown'=>true,
+    'hideInput'=>true
 ]);
+echo '</div></div>';
+
+?>
+<!--<div class="form-group">-->
+<div class="col-md-4">
+<?= Html::submitButton(Yii::t('app', '查询'), ['class' => 'btn-lg btn-success']) ?>
+    <?php
+//        echo  Html::submitButton('查询', ['id' => 'date-str', 'class' => 'btn btn-primary '])
+         ;?>
+
+</div>
+</div></br></br>
+<?php ActiveForm::end(); ?>
+<?php
+$dataArr = ['自主开发+销售推荐' => $dataProvider,'自主开发' => $dataProvider3];
+foreach ($dataArr as $key=>$value){
+    echo GridView::widget([
+        'dataProvider' => $value,
+        'showPageSummary'=>true,
+        'pjax'=>true,
+        'export' => false,
+        'striped'=>true,
+        'hover'=>true,
+        'panel'=>['type'=>'primary', 'heading'=>'采购开发产品统计--来源'.$key],
+        'columns'=>[
+            ['class'=>'kartik\grid\SerialColumn'],
+
+            [
+                'attribute'=>'purchaser',
+                'pageSummary'=>'Page Summary',
+                'pageSummaryOptions'=>['class'=>'text-right text-warning'],
+            ],
+            [
+                'attribute'=>'reject',
+                'label'=>'拒绝',
+                'width'=>'150px',
+                'hAlign'=>'right',
+                'format'=>['decimal', 0],
+                'pageSummary'=>true
+            ],
+            [
+                'attribute'=>'get',
+                'label'=>'拿样',
+                'width'=>'150px',
+                'hAlign'=>'right',
+                'format'=>['decimal', 0],
+                'pageSummary'=>true
+            ],
+            [
+                'attribute'=>'need',
+                'label'=>'需议价或谈其他条件',
+                'width'=>'150px',
+                'hAlign'=>'right',
+                'format'=>['decimal', 0],
+                'pageSummary'=>true
+            ],
+            [
+                'attribute'=>'undo',
+                'label'=>'未评审',
+                'width'=>'150px',
+                'hAlign'=>'right',
+                'format'=>['decimal', 0],
+                'pageSummary'=>true
+            ],
+            [
+                'attribute'=>'direct',
+                'label'=>'直接下单',
+                'width'=>'150px',
+                'hAlign'=>'right',
+                'format'=>['decimal', 0],
+                'pageSummary'=>true
+            ],[
+                'attribute'=>'season',
+                'label'=>'季节产品推迟',
+                'width'=>'150px',
+                'hAlign'=>'right',
+                'format'=>['decimal', 0],
+                'pageSummary'=>true
+            ],
+            [
+                'class'=>'kartik\grid\FormulaColumn',
+                'header'=>'产品总数',
+                'value'=>function ($model, $key, $index, $widget) {
+                    $p = compact('model', 'key', 'index');
+                    return $widget->col(2, $p) + $widget->col(3, $p)+
+                        $widget->col(4, $p)+ $widget->col(5, $p)+ $widget->col(6, $p)+ $widget->col(7, $p);
+                },
+                'mergeHeader'=>true,
+                'width'=>'150px',
+                'hAlign'=>'right',
+                'format'=>['decimal', 0],
+                'pageSummary'=>true
+            ],
+            [
+                'class'=>'kartik\grid\FormulaColumn',
+                'header'=>'拒绝率%',
+                'value'=>function ($model, $key, $index, $widget) {
+                    $p = compact('model', 'key', 'index');
+                    return $widget->col(2, $p) / $widget->col(8, $p) *100;
+                },
+                'mergeHeader'=>true,
+                'width'=>'150px',
+                'hAlign'=>'right',
+                'format'=>['decimal', 2],
+//            'pageSummary'=>true,
+                'pageSummaryFunc'=>GridView::F_AVG,
+            ],
+            [
+                'class'=>'kartik\grid\FormulaColumn',
+                'header'=>'成功率%',
+                'value'=>function ($model, $key, $index, $widget) {
+                    $p = compact('model', 'key', 'index');
+                    return ($widget->col(3, $p)+ $widget->col(6, $p))/ $widget->col(8, $p) *100;
+                },
+                'mergeHeader'=>true,
+                'width'=>'150px',
+                'hAlign'=>'right',
+                'format'=>['decimal', 2],
+//            'pageSummary'=>true,
+                'pageSummaryFunc'=>GridView::F_AVG,
+
+            ],
+        ],
+    ]);
+}
+
 echo GridView::widget([
     'dataProvider' => $dataProvider2,
     'showPageSummary'=>true,
@@ -198,7 +232,7 @@ echo GridView::widget([
             'header'=>'拒绝率%',
             'value'=>function ($model, $key, $index, $widget) {
                 $p = compact('model', 'key', 'index');
-                return $widget->col(2, $p) / $widget->col(8, $p) *100;
+                return ($widget->col(8, $p) *100>0)?$widget->col(2, $p) / $widget->col(8, $p) *100:0;
             },
             'mergeHeader'=>true,
             'width'=>'150px',
@@ -212,7 +246,8 @@ echo GridView::widget([
             'header'=>'成功率%',
             'value'=>function ($model, $key, $index, $widget) {
                 $p = compact('model', 'key', 'index');
-                return ($widget->col(3, $p)+ $widget->col(6, $p))/ $widget->col(8, $p) *100;
+
+                return ($widget->col(8, $p) *100>0)?(($widget->col(3, $p)+ $widget->col(6, $p))/ $widget->col(8, $p) *100):0;
             },
             'mergeHeader'=>true,
             'width'=>'150px',
@@ -256,3 +291,25 @@ echo GridView::widget([
         ],
     ],
 ]);
+?>
+<?php
+    $submit = Url::toRoute('compute');
+    $js =<<<JS
+    $('#date-str').on('click',function() {
+      var button = $(this);
+      button.attr('disabled','disabled');
+      var date_range = $('#w1').val();
+      console.log(date_range);
+      $.ajax({
+          url:'{$submit}',
+          type: 'post',
+          data:{date_range:date_range},
+          success:function(data) {
+            button.attr('disabled',false);
+          }
+      });
+    });
+JS;
+
+//$this->registerJs($js);
+?>
